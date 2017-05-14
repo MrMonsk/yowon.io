ccb8eda259482c9ee70dc1000eaa612c35c162cb
yoonster <yowonyoon@gmail.com>
2017-05-14 17:12:54 -0400

    initial commit

---
+++ components/Box.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import FontAwesome from 'react-fontawesome';
+
+export default class extends Component {
+    constructor (props) {
+        super(props);
+    }
+
+    render () {
+        const { myName } = this.props;
+        return (
+            <div 
+                style = {
+                    { 
+                        display: 'flex', 
+                        alignItems: 'center', 
+                        justifyContent: 'center', 
+                        height: '100vh'
+                    }
+                }
+            >
+                <div className='box'
+                    style = {
+                        {
+                            height: '50vh',
+                            width: '50vw',
+                            backgroundColor: '#f45c41',
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            flexDirection: 'column'
+                        }
+                    }
+                >
+                    <h1 style={{color: 'white' }}>{myName}</h1>
+                    <p style={{color: 'white'}}>Dank memes and the 2K dream.</p>
+                    <div className='icon-list' style={{color: 'white', display: 'flex', flexDirection: 'row'}}>
+                        <a style={{textDecoration: 'none', color: 'white', paddingRight: 10}} href="https://github.com/mrmonsk" target="_blank"><i className="fa fa-github-alt fa-2x" /></a>
+                        <a style={{textDecoration: 'none', color: 'white'}} href="mailto:yowonyoon@gmail.com?Subject=Hello"><i className="fa fa-envelope fa-2x" /></a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Head from 'next/head';
+import Box from '../components/Box';
+
+export default class extends Component {
+    constructor () {
+        super ()
+    }
+
+    render () {
+        return (
+            <div>
+                <Head>
+                    <title>yowon.io</title>
+                    <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+                    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" />
+                    <script src="https://use.fontawesome.com/6f29465722.js" />
+                </Head>
+                <style jsx>
+                    {`
+                        * {
+                            font-family: 'Open Sans', sans-serif;
+                        }
+                    `}
+                </style>
+                <Box myName='Yowon Yoon' />
+            </div>
+        )
+    }
+}
